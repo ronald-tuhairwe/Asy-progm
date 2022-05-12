dd7330e15bf6f8b0d5b7a5de682a28c5899298c4
mporogomankadde <tuhairweronnie@gmail.com>
2022-05-12 08:26:58 -0500

    final

--- DsgnPtners/lab7/index.js
+++ DsgnPtners/lab7/index.js
@@ -1,30 +1,24 @@
 /*eslint-disable*/
-'use strict'
+"use strict";
 
-window.onload=function(){
+window.onload = function () {
+  let res = async function () {
+    let wrapper = document.getElementById("wrp");
+    let res = await fetch("https://randomuser.me/api/?results=5");
+    let commit = await res.json();
 
-
- let res=function (){
-        let wrapper=document.getElementById("wrp"); 
-        fetch('https://randomuser.me/api/?results=5')
-        .then(response => response.json())
-        .then(commit => {
-        console.log(commit.results);
-        commit.results.forEach(elem =>{
-         
-        wrapper.innerHTML  += `<div id="img">
+    wrapper.innerHTML = "";
+    commit.results.forEach((elem) => {
+      wrapper.innerHTML += `<div id="img">
         <img src="${elem.picture.medium}"> </div>
         <div id="name">${elem.name.first} ${elem.name.last}</div>
         <div id="phone">${elem.phone}</div>
-        <div id="email">${elem.email}</div>`   
-        })
-    
-        })
-        wrapper.innerHTML ="";
-    }
-  res();
-    
-let btn =document.getElementById('btn');
-btn.onclick= res;
+        <div id="email">${elem.email}</div>`;
+    });
+  };
 
-}
+  res();
+
+  let btn = document.getElementById("btn");
+  btn.onclick = res;
+};
